d5d32b489cfc39778802e6852fa0adf54d655008
Kyle <koichi@ChengLichun-no-MacBook-Air.local>
2017-02-21 00:13:19 +0900

    added .zpreztorc

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ ln -si ~/dotfiles/dein.toml ~/.config/nvim/dein.toml
 ln -si ~/dotfiles/deinlazy.toml ~/.config/nvim/deinlazy.toml
 ln -si ~/dotfiles/.zshrc ~/.zshrc
 ln -si ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -si ~/dotfiles/.zpreztorc ~/.zpreztorc
